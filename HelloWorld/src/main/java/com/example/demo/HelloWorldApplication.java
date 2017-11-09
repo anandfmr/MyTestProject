1f71ab4d46979a79790178b4696c6a8205132a1e
Anand <anand.ksingh@yandex.com>
2017-11-09 19:37:31 +0530

    V1

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
+	@RequestMapping("/Messages")
+	String home()
+	{
+		return "Hello Anand!! How are you ?";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
